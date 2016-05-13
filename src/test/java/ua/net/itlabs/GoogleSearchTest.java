6cc4faa27ac4f4b4de7de3ed0a0f529379251d78
ljubanka <lvpopl@gmail.com>
2016-05-13 14:46:58 +0300

    changes after review

---
+++ src/test/java/ua/net/itlabs/GoogleSearchTest.java
@@ -0,0 +1,37 @@
+package ua.net.itlabs;
+
+import com.codeborne.selenide.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.*;
+import static org.junit.Assert.assertEquals;
+
+public class GoogleSearchTest {
+    
+    @Test
+    public void testSearchAndFollowLink() {
+        open("http://google.com/ncr");
+        startSearch("Selenium automates browsers").pressEnter();
+        searchResults.shouldHaveSize(10);
+        searchResults.first().shouldHave(text("Selenium automates browsers"));
+        followNthLink(0);
+        $("#header>h1>a").shouldHave(exactText("Browser Automation"));
+        assertEquals("http://www.seleniumhq.org/", url());
+    }
+
+    ElementsCollection searchResults = $$(".srg>.g");
+
+    public SelenideElement startSearch(String text) {
+        return $(By.name("q")).setValue(text);
+    }
+
+    public void followNthLink(int index) {
+        searchResults.get(index).$(".r>a").click();
+    }
+
+}
